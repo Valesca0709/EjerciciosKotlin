cce5c19e7f30db6a1465dd654f33f2d2d19327b0
Valesca <valesca.torres07@gmail.com>
2022-12-20 15:13:28 -0300

    update

--- app/src/main/java/com/androidenavanzado/ejercicioskotlin/MainActivity.kt
+++ app/src/main/java/com/androidenavanzado/ejercicioskotlin/MainActivity.kt
@@ -7,23 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        miniMaxSum()
 
     }
 
-    fun miniMaxSum(): Unit {
 
-        val nums: Array<Int> = arrayOf(1, 3, 5, 7, 9)
-        var contador = 1 // el contador tiene que ir sumando uno para lograr la suma
-        var suma = 0
-
-        nums.forEachIndexed { index, numero ->
-            if (index != contador){
-                suma += numero
-
-            }
-        }
-
-        print("El valor es la suma es $suma")
-    }
 }
